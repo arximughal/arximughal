6c6f3ccd035f56ddceeae0c5df5298ae984a5c09
Muhammad Arslan Aslam <hello@arslanaslam.me>
2024-08-31 04:28:28 +0500

    fix the Google Analytics script tag in siteMetaData.ts

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,10 +18,28 @@ const siteMetadata = {
   linkedin: 'https://pk.linkedin.com/in/marslanaslam',
   locale: 'en-US',
   analytics: {
-    // supports plausible, simpleAnalytics or googleAnalytics
-    // plausibleDataDomain: 'https://bytes.arslanaslam.me', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    googleAnalyticsId: 'G-L2PFY01RMW', // e.g. UA-000000-2 or G-XXXXXXX
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    umamiAnalytics: {
+      // We use an env variable for this site to avoid other users cloning our analytics ID
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      // You may also need to overwrite the script if you're storing data in the US - ex:
+      // src: 'https://us.umami.is/script.js'
+      // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
+    },
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // If you are hosting your own Plausible.
+    //   src: '', // e.g. https://plausible.my-domain.com/js/script.js
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    googleAnalytics: {
+      googleAnalyticsId: 'G-L2PFY01RMW', // e.g. G-XXXXXXX
+    },
   },
   comment: {
     // Select a provider and use the environment variables associated to it
